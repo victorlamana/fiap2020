180e6c102c800732cb98186d494d2469060ad033
victorlamana <victor.lamanadev@gmail.com>
2020-09-01 22:47:42 -0300

    inline return

--- MMPython/MM4_test.py
+++ MMPython/MM4_test.py
@@ -1,28 +1,41 @@
-def funcao1():
-    print("PORRA")
+def rogerio(qtd,texto):
+    for contador in range(0,qtd):
+        print(texto)
 
-def funcao2():
-    print("")
+def funcao1(qtd):
+    for contador in range(0,qtd,1):
+        print("PORRA")
+    return  
 
-def funcao3():
-    print("")
+def funcao2(qtd):
+    return [print("dbfdhfbs") for contador in range(0,qtd)
 
-def funcao4():
-    print("")
+def funcao3(qtd):
+    for contador in range(0,qtd,1):
+        print("adsdsa")
+    return  
+
+def funcao4(qtd):
+    for contador in range(0,qtd,1):
+        print("aaaaa")
+    return  
 
 e='t'
 
 while e!='S':
     print ("Escolha conforme menu:")
     e=input("A para função1\nbpara função 2\ncpara função3\nd para função4\ns para sair\n").upper()
+
+    qtd=int(input("quantas impressoes vc deseja\n"))
+
     if e=='A':
-        funcao1()
+        funcao1(qtd)
     elif e=='B':
-        funcao2()
+        funcao2(qtd)
     elif e=='C':
-        funcao3()
+        funcao3(qtd)
     elif e=='D':
-        funcao4()
+        funcao4(qtd)
     elif e=='S':
         pass
     else:
