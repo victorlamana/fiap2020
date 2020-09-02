f576e36267dadb73aad3de543df3488d2d379190
victorlamana <victor.lamanadev@gmail.com>
2020-09-01 23:56:50 -0300

    nacs finish

---
+++ MMPython/NAC1-2.py
@@ -0,0 +1,20 @@
+contador = 0
+XF=0 
+s = 0
+s_lista = []
+
+while (XF<=0):
+    XF=int(input("Informe um valor inteiro e positivo:"))
+
+for contador in range(contador,XF,1):
+    s = 3*contador
+    s_lista.append(s)
+
+print ("Série:", s_lista)
+
+    
+
+
+
+
+
